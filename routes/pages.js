994011407908ba21eb4075bd924a9150d788add8
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2025-02-04 00:43:31 +0100

    Changed home page back to wallet connect page

--- routes/pages.js
+++ routes/pages.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 // Home page
 router.get("/", (req, res) => {
-  res.render("404", {
+  res.render("connectWallet", {
     walletAddress: process.env.WALLET_ADDRESS,
     title: "Solana Giveaway",
     alert: false,
